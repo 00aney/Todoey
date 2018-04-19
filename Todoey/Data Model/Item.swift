0f516fa112e6bf18f386cfaee15ee2cc10988592
aney <yenafirst91@gmail.com>
2018-04-19 02:27:50 -0400

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by aney on 2018. 4. 18..
+//  Copyright © 2018년 Ted Kim. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+  var title: String = ""
+  var done: Bool = false
+}
