388c6878c7cbe2319f1e41718027e384cb715326
aney <yenafirst91@gmail.com>
2018-04-26 00:19:13 -0400

    Add category feature

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -8,33 +8,44 @@
 
 import UIKit
 
+import RealmSwift
+
+
 class TodoListViewController: UITableViewController {
 
-  var itemArray = [Item]()
-  let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+  @IBOutlet weak var searchBar: UISearchBar!
+  
+  var todoItems: Results<Item>?
+  let realm = try! Realm()
+  
+  var selectedCategory: Category? {
+    didSet {
+      loadItems()
+    }
+  }
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
-//    print(dataFilePath)
-    
-    loadItems()
+    searchBar.delegate = self
   }
   
   
   // MARK: - TableView Datasource Methods
 
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return itemArray.count
+    return todoItems?.count ?? 1
   }
   
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-    let item = itemArray[indexPath.row]
+    if let item = todoItems?[indexPath.row] {
+      cell.textLabel?.text = item.title
+      cell.accessoryType = item.done ? .checkmark : .none
+    } else {
+      cell.textLabel?.text = "No Items Added"
+    }
     
-    cell.textLabel?.text = item.title
-    cell.accessoryType = item.done ? .checkmark : .none
-
     return cell
   }
   
@@ -42,9 +53,18 @@ class TodoListViewController: UITableViewController {
   // MARK: - TableView Delegate Methods
   
   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    itemArray[indexPath.row].done = !itemArray[indexPath.row].done
     
-    saveItems()
+    if let item = todoItems?[indexPath.row] {
+      do {
+        try realm.write {
+          item.done = !item.done
+        }
+      } catch {
+        print("Error saving done status, \(error)")
+      }
+    }
+    
+    tableView.reloadData()
     
     tableView.deselectRow(at: indexPath, animated: true)
   }
@@ -59,11 +79,19 @@ class TodoListViewController: UITableViewController {
     
     let action = UIAlertAction(title: "Add Item", style: .default) { action in
       
-      let newItem = Item()
-      newItem.title = textField.text!
-      self.itemArray.append(newItem)
-      
-      self.saveItems()
+      if let currentCategory = self.selectedCategory {
+        do {
+          try self.realm.write {
+            let newItem = Item()
+            newItem.title = textField.text!
+            newItem.dateCreated = Date()
+            currentCategory.items.append(newItem)
+          }
+        } catch {
+          print("Error saving new items, \(error)")
+        }
+      }
+      self.tableView.reloadData()
     }
     
     alert.addTextField { alertTextField in
@@ -75,28 +103,33 @@ class TodoListViewController: UITableViewController {
     present(alert, animated: true)
   }
   
-  func saveItems() {
-    let encoder = PropertyListEncoder()
-    do {
-      let data = try encoder.encode(itemArray)
-      try data.write(to: dataFilePath!)
-    } catch {
-      print("Error encoding item array, \(error)")
-    }
-    
+  func loadItems() {
+    todoItems = selectedCategory?.items.sorted(byKeyPath: "title", ascending: true)
     tableView.reloadData()
   }
   
-  func loadItems() {
-    if let data = try? Data(contentsOf: dataFilePath!) {
-      let decoder = PropertyListDecoder()
-      do {
-        itemArray = try decoder.decode([Item].self, from: data)
-      } catch {
-        print("Error decoding item array, \(error)")
+}
+
+
+// MARK: - UISearchBarDelegate
+
+extension TodoListViewController: UISearchBarDelegate {
+  
+  func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+    
+    todoItems = todoItems?.filter("title CONTAINS[cd] %@", searchBar.text!)
+      .sorted(byKeyPath: "dateCreated", ascending: true)
+    tableView.reloadData()
+  }
+  
+  func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+    if searchBar.text?.count == 0 {
+      loadItems()
+      
+      DispatchQueue.main.async {
+        searchBar.resignFirstResponder()
       }
     }
   }
   
 }
-
--- Todoey/Data Model/Item.swift
+++ Todoey/Data Model/Item.swift
@@ -2,13 +2,18 @@
 //  Item.swift
 //  Todoey
 //
-//  Created by aney on 2018. 4. 18..
+//  Created by aney on 2018. 4. 22..
 //  Copyright © 2018년 Ted Kim. All rights reserved.
 //
 
 import Foundation
 
-class Item: Codable {
-  var title: String = ""
-  var done: Bool = false
+import RealmSwift
+
+
+class Item: Object {
+  @objc dynamic var title: String = ""
+  @objc dynamic var done: Bool = false
+  @objc dynamic var dateCreated: Date?
+  var parentCategory = LinkingObjects(fromType: Category.self, property: "items")
 }
